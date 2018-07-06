f6a3f1966f5cc110fc1199c675019869e1b14648
14-09-1998 <40885664+14-09-1998@users.noreply.github.com>
2018-07-06 15:02:53 +0530

    Create udpechoclient.py

---
+++ udpechoclient.py
@@ -0,0 +1,11 @@
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NO=6998
+Message=("Hello Server")
+bytesToSend = str.encode(Message)
+clientSock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+clientSock.sendto(bytesToSend ,(UDP_IP_ADDRESS,UDP_PORT_NO))  
+msgFromServer=clientSock.recvfrom(1024)
+print(msgFromServer)
+
+   
