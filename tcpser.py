6d98f2a2002f3e618634fb8c62c0164b97a7ce67
14-09-1998 <40885664+14-09-1998@users.noreply.github.com>
2018-07-13 15:39:21 +0530

    Create tcpser.py

---
+++ tcpser.py
@@ -0,0 +1,21 @@
+import socket
+localIP="127.0.0.1"
+localPort=20000
+bufferSize=1024
+msgFromServer="HELLO TCP CLIENT"
+bytesToSend=str.encode(msgFromServer)
+TCPServerSocket=socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
+TCPServerSocket.bind((localIP,localPort))
+TCPServerSocket.listen(1)
+print("TCP server up and listening")
+TCPServerSocket,address=TCPServerSocket.accept()
+while True:
+  msg= TCPServerSocket.recv(2048).decode()
+  print('>> ',msg)
+  message = input(">> ")
+  TCPServerSocket.send(message.encode())
+  if(message == 'q'):
+    TCPServerSocket.close()
+
+	
+	
